48c7fd30654553f174607da6faec5f98fbcdedc4
lborsini <122235229+lborsini@users.noreply.github.com>
2023-03-04 17:27:25 +0000

    implemetation UI in application

--- appVisor.py
+++ appVisor.py
@@ -1,22 +1,32 @@
 from tkinter import *
 from PIL import Image, ImageTk
+from tkinter import filedialog
 
 root = Tk() 
-root.title('Visor de Imagenes')
+root.title('Photo Viewer')
+root.configure(background='#212121')
+root.geometry('760x750')
+root.resizable(0,0)
 
-i1 = ImageTk.PhotoImage(Image.open('Img/pexels-alexander-grey-1148998.jpg'))
-i2 = ImageTk.PhotoImage(Image.open('Img/pexels-benjamin-suter-3617500.jpg'))
-i3 = ImageTk.PhotoImage(Image.open('Img/pexels-eberhard-grossgasteiger-1366919.jpg'))
-i4 = ImageTk.PhotoImage(Image.open('Img/pexels-eberhard-grossgasteiger-443446.jpg'))
-i5 = ImageTk.PhotoImage(Image.open('Img/pexels-kehn-hermano-3849167.jpg'))
-i6 = ImageTk.PhotoImage(Image.open('Img/pexels-mo-eid-9454915.jpg'))
-i7 = ImageTk.PhotoImage(Image.open('Img/pexels-pixabay-268533.jpg'))
-i8 = ImageTk.PhotoImage(Image.open('Img/pexels-tiff-ng-3700245.jpg'))
+frame = Frame(root, width=700, height=650)
+frame.grid(row=0, column=0, columnspan=4, padx=30, pady=30)
+frame.config(background='#212121')
 
-lst=[i1, i2, i3, i4, i5, i6, i7, i8]
+# fisrt 4 img its a demostration
+i1 = Image.open('Img/pexels-alexander-grey-1148998.jpg').resize((700, 600), Image.ANTIALIAS)
+i2 = Image.open('Img/pexels-benjamin-suter-3617500.jpg').resize((700, 600), Image.ANTIALIAS)
+i3 = Image.open('Img/pexels-eberhard-grossgasteiger-443446.jpg').resize((700, 600), Image.ANTIALIAS)
+i4 = Image.open('Img/pexels-kehn-hermano-3849167.jpg').resize((700, 600), Image.ANTIALIAS)
 
-l = Label(root, image = i1)
-l.grid(row = 0, column = 0, columnspan = 3 )
+lst=[i1, i2, i3, i4]
+list = []
+
+for i in lst:
+    img = ImageTk.PhotoImage(i)
+    list.append(img)
+
+l = Label(frame, image = list[0])
+l.grid(row = 0, column = 0, columnspan = 3, sticky='nswe')
 
 def adelante(imgNum):
     global l
@@ -24,16 +34,18 @@ def adelante(imgNum):
     global btnAdelante
 
     l.grid_forget()
-    l = Label(root, image=lst[imgNum])
-    btnAtras = Button(root, text= ' <- ', command= lambda: atras(imgNum - 1 ))
-    btnAdelante = Button(root, text = ' -> ', command= lambda: adelante(imgNum + 1))
+    l = Label(frame, image=list[imgNum])
+    btnAtras = Button(root, text= ' <- ', bd=5, fg='#222212', bg='#EEE', height=1, width=11, command= lambda: atras(imgNum - 1 ))
+    btnAdelante = Button(root, text = ' -> ', bd=5, fg='#222212', bg='#EEE', height=1, width=11, command= lambda: adelante(imgNum + 1))
     
-    if imgNum == (len(lst) - 1):
+    if imgNum == (len(list) - 1):
         btnAdelante = Button(root, text= 'N/A', state=DISABLED)
+        btnAtras.focus()
 
-    l.grid(row = 0, column = 0, columnspan = 3 )
-    btnAtras.grid(row = 1, column = 0)
-    btnAdelante.grid(row = 1, column = 2)
+    l.grid(row = 0, column = 0, sticky='nswe')
+    btnAtras.grid(row = 2, column = 0, padx=100, pady=20)  
+    btnAdelante.grid(row = 2, column = 2, padx=100, pady=20)
+    btnAdelante.focus()
 
 def atras(imgNum):
     global l
@@ -41,22 +53,25 @@ def atras(imgNum):
     global btnAdelante
 
     l.grid_forget()
-    l = Label(root, image=lst[imgNum])
-    btnAtras = Button(root, text= ' <- ', command= lambda: atras(imgNum - 1 ))
-    btnAdelante = Button(root, text = ' -> ', command= lambda: adelante(imgNum + 1))
-    
+    l = Label(frame, image=list[imgNum])
+    btnAtras = Button(root, text= ' <- ', bd=5, fg='#222212', bg='#EEE', height=1, width=11, command= lambda: atras(imgNum - 1 ))
+    btnAdelante = Button(root, text = ' -> ', bd=5, fg='#222212', bg='#EEE', height=1, width=11, command= lambda: adelante(imgNum + 1))
+
     if imgNum == 0:
         btnAtras = Button(root, text= 'N/A', state=DISABLED)
+        btnAdelante.focus()
 
-    l.grid(row = 0, column = 0, columnspan = 3 )
-    btnAtras.grid(row = 1, column = 0)
-    btnAdelante.grid(row = 1, column = 2)
+    l.grid(row = 0, column = 0, sticky='nswe')
+    btnAtras.grid(row = 2, column = 0, padx=100, pady=20)  
+    btnAdelante.grid(row = 2, column = 2, padx=100, pady=20)
+    btnAtras.focus()
 
-btnAtras = Button(root, text= 'N/A', state=DISABLED)
-btnAdelante = Button(root, text = ' -> ', command= lambda: adelante(1))
+btnAtras = Button(root, text= 'N/A', state=DISABLED, bd=5, fg='#222212', bg='#EEE', height=1, width=11)
+btnAtras.grid(row = 2, column = 0, padx=100, pady=20)
 
-btnAtras.grid(row = 1, column = 0)
-btnAdelante.grid(row = 1, column = 2)
+btnAdelante = Button(root, text = ' -> ', command= lambda: adelante(1), bd=5, fg='#222212', bg='#EEE', height=1, width=11)
+btnAdelante.grid(row = 2, column = 2, padx=100, pady=20)
 
+btnAdelante.focus()
 
 root.mainloop()
